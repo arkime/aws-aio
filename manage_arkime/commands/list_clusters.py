430891005f162fdabf3b00da74a49e461be8ef1f
Chris Helma <chelma+github@amazon.com>
2023-04-20 13:12:36 -0500

    list-clusters now reports the VNI for each monitored VPC
    Signed-off-by: Chris Helma <chelma+github@amazon.com>

--- manage_arkime/commands/list_clusters.py
+++ manage_arkime/commands/list_clusters.py
@@ -18,9 +18,18 @@ def cmd_list_clusters(profile: str, region: str) -> List[Dict[str, str]]:
     for cluster_name in cluster_names:
         ssm_vpcs_path_prefix = f"{constants.get_cluster_ssm_param_name(cluster_name)}/vpcs"
         vpc_ids = ssm_ops.get_ssm_names_by_path(ssm_vpcs_path_prefix, aws_provider)
+        vpc_details = []
+        for vpc_id in vpc_ids:
+            vpc_ssm_param = constants.get_vpc_ssm_param_name(cluster_name, vpc_id)
+            vni = ssm_ops.get_ssm_param_json_value(vpc_ssm_param, "mirrorVni", aws_provider)
+            vpc_details.append({
+                "vpc_id": vpc_id,
+                "vni": vni,
+            })
+
         cluster_details.append({
             "cluster_name": cluster_name,
-            "monitored_vpcs": vpc_ids
+            "monitored_vpcs": vpc_details
         })
 
     formatted_details = json.dumps(cluster_details, indent=4)
